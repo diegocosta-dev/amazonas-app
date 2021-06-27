22f6ecab69158dafad6b8f25bfde66bb0c3e5c70
Diego Costa <troiablender@gmail.com>
2021-06-27 09:49:23 -0300

    feat create product and rating component

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -4,11 +4,11 @@ const data = {
             _id: "1",
             name: "Nike Slim Shirt",
             category: "Shirts",
-            image: "./images/p1.jpg",
+            image: "../images/p1.jpg",
             price: 120,
             brand: "Nike",
             rating: 4.5,
-            numReviws: 10,
+            numReviews: 10,
             description: "High quality product"
 
         },
@@ -16,11 +16,11 @@ const data = {
             _id: "2",
             name: "Adidas Fit Shirt",
             category: "Shirts",
-            image: "./images/p2.jpg",
+            image: "../images/p2.jpg",
             price: 100,
             brand: "Adidas",
             rating: 4.0,
-            numReviws: 10,
+            numReviews: 20,
             description: "High quality product"
 
         },
@@ -28,11 +28,11 @@ const data = {
             _id: "3",
             name: "Lacost Free Shirt",
             category: "Shirts",
-            image: "./images/p3.jpg",
+            image: "../images/p3.jpg",
             price: 150,
             brand: "Lacost",
             rating: 4.9,
-            numReviws: 10,
+            numReviews: 25,
             description: "High quality product"
 
         },
@@ -40,11 +40,11 @@ const data = {
             _id: "4",
             name: "Nike Slim Pants",
             category: "Pants",
-            image: "./images/p4.jpg",
+            image: "../images/p4.jpg",
             price: 78,
             brand: "Nike",
             rating: 4.2,
-            numReviws: 10,
+            numReviews: 30,
             description: "High quality product"
 
         },
@@ -52,11 +52,11 @@ const data = {
             _id: "5",
             name: "Puma Slim Shirt",
             category: "Pants",
-            image: "./images/p5.jpg",
+            image: "../images/p5.jpg",
             price: 120,
             brand: "Puma",
             rating: 3.0,
-            numReviws: 10,
+            numReviews: 40,
             description: "High quality product"
 
         },
@@ -64,11 +64,11 @@ const data = {
             _id: "6",
             name: "Adidas Fit Pants",
             category: "Pants",
-            image: "./images/p6.jpg",
+            image: "../images/p6.jpg",
             price: 120,
             brand: "Adidas",
             rating: 4.0,
-            numReviws: 15,
+            numReviews: 50,
             description: "High quality product"
 
         }
