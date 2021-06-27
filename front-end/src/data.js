b0e1b3927a12c302c1f307c6cb2472264bcc7668
Diego Costa <troiablender@gmail.com>
2021-06-27 08:19:31 -0300

    share code on github

---
+++ front-end/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Nike Slim Shirt",
+            category: "Shirts",
+            image: "./images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviws: 10,
+            description: "High quality product"
+
+        },
+        {
+            _id: "2",
+            name: "Adidas Fit Shirt",
+            category: "Shirts",
+            image: "./images/p2.jpg",
+            price: 100,
+            brand: "Adidas",
+            rating: 4.0,
+            numReviws: 10,
+            description: "High quality product"
+
+        },
+        {
+            _id: "3",
+            name: "Lacost Free Shirt",
+            category: "Shirts",
+            image: "./images/p3.jpg",
+            price: 150,
+            brand: "Lacost",
+            rating: 4.9,
+            numReviws: 10,
+            description: "High quality product"
+
+        },
+        {
+            _id: "4",
+            name: "Nike Slim Pants",
+            category: "Pants",
+            image: "./images/p4.jpg",
+            price: 78,
+            brand: "Nike",
+            rating: 4.2,
+            numReviws: 10,
+            description: "High quality product"
+
+        },
+        {
+            _id: "5",
+            name: "Puma Slim Shirt",
+            category: "Pants",
+            image: "./images/p5.jpg",
+            price: 120,
+            brand: "Puma",
+            rating: 3.0,
+            numReviws: 10,
+            description: "High quality product"
+
+        },
+        {
+            _id: "6",
+            name: "Adidas Fit Pants",
+            category: "Pants",
+            image: "./images/p6.jpg",
+            price: 120,
+            brand: "Adidas",
+            rating: 4.0,
+            numReviws: 15,
+            description: "High quality product"
+
+        }
+    ]
+}
+
+export default data;
